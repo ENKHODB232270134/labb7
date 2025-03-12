7e96c70509a9f8be1a006edae3a899493c329c59
ENKHODB232270134 <notsyniz@gmail.com>
2025-03-12 15:48:34 +0800

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -1,4 +1,4 @@
-a = int(input())
+a = float(input())
 
 if a <= 100:
    b = 5
@@ -6,11 +6,12 @@ elif a < 1000:
    b = a * 0.2
 else:
    b =a * 0.1
+if (a % 10 == 5):
+    c = b+2
+elif (a % 2 == 0):
+    c = b+1
+else:
+    c = b
+    print(c)
 
-if a % 2 == 0:
-    b += 1
-if a % 10 == 5:
-    b += 2
-
-f = a + b
-print(f"\n{a} \n{b} \n{f}")
+    print(a+c)
